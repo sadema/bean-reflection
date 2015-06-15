4c9a850dc56616f7dd4c53e43627b7d6007433d3
sjoerdadema <sjoerd@kristalsoftware.nl>
2015-06-15 16:24:58 +0200

    test non existent fieldname in a bean

--- test/main/java/nl/kristalsoftware/beanreflection/main/BeanReflectionUtilsImplTest.java
+++ test/main/java/nl/kristalsoftware/beanreflection/main/BeanReflectionUtilsImplTest.java
@@ -7,17 +7,26 @@ import org.junit.Test;
 import java.lang.reflect.Field;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 /**
  * Created by sjoerdadema on 15-06-15.
  */
 public class BeanReflectionUtilsImplTest {
-
+    
     @Test
-    public void testGetField() throws Exception {
+    public void testGetProductidField() throws Exception {
         ProductData productData = new ProductData("123");
         BeanReflectionUtils utils = new BeanReflectionUtilsImpl();
         Field f = utils.getField(productData, "productid");
         assertNotNull("field is null", f);
+        assertThat("name is not productid", f.getName(), is("productid"));
+    }
+
+    @Test(expected = NoSuchFieldException.class)
+    public void testThrowNoSuchFieldException() throws Exception {
+        ProductData productData = new ProductData("123");
+        BeanReflectionUtils utils = new BeanReflectionUtilsImpl();
+        Field f = utils.getField(productData, "NonExistentFieldname");
     }
 }
